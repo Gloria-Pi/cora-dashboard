61d503bbc0b4327e3cd01cc0ca0b7efd6a991d70
Gloria-Pi <gloria.paita@edu-its.it>
2026-02-22 20:27:17 +0100

    feat[MAIN]: first commit and implementation

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+// import "./index.css"; // Importing Tailwind CSS and custom styles
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Autoprefixer Test</h1>
+      <div className="grid-container">
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+      </div>
+      <p>Ciao!</p>
+    </div>
+  );
+}
+
+export default App;
