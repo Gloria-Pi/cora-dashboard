403c3efa17b3118e5477deeaa026ab07aa4199d9
Gloria-Pi <gloria.paita@edu-its.it>
2026-02-26 18:05:39 +0100

    feat[CARD]: refactored styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router/dom';
-import './index.css';
-import { router } from './app/router';
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router/dom";
+
+import { router } from "./app/router";
+import "./styles/index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
