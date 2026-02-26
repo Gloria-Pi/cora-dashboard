7fbbc3f663e3e2279a07b0cfa81f35e868bd7a82
Gloria-Pi <gloria.paita@edu-its.it>
2026-02-26 16:39:28 +0100

    feat[ROUTER]: modified options in prettierrc, then modified style of routing pages accordingly

--- src/app/layout/DashboardLayout.tsx
+++ src/app/layout/DashboardLayout.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router';
-import Sidebar from '../../components/Sidebar/Sidebar';
+import { Outlet } from "react-router";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export default function DashboardLayout() {
   return (
