d636185ffaa29919d9e0115a3f4f8dcb115ca1fc
Gloria-Pi <gloria.paita@edu-its.it>
2026-04-20 15:29:25 +0200

    fix[AI]: test for hosting service

--- src/app/pages/Trends.tsx
+++ src/app/pages/Trends.tsx
@@ -1,17 +1,17 @@
-import { useData } from "../../hooks/useData";
+// import { useData } from "../../hooks/useData";
 
 export default function Trends() {
-  const data = useData();
-  console.log(data);
+  // const data = useData();
+  // console.log(data);
 
-  const SchroedingerData = data.data
-    ? JSON.stringify(data.data)
-    : `${data.error}`;
+  // const SchroedingerData = data.data
+  //   ? JSON.stringify(data.data)
+  //   : `${data.error}`;
 
   return (
     <>
       <h1 className="text-2xl font-bold">Trends Page</h1>
-      <div>{SchroedingerData}</div>
+      {/* <div>{SchroedingerData}</div> */}
     </>
   );
 }
